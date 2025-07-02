93855b8602386ca782fd6e05b08b368d17e0efe1
Shahzeb-Abro <khizr772@gmail.com>
2025-07-02 23:24:46 +0500

    Translated typography from figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="text-4xl text-emerald-500">Hello World</h1>;
+  return <h1 className="text-preset-9 text-emerald-500">Hello World</h1>;
 }
 
 export default App;
