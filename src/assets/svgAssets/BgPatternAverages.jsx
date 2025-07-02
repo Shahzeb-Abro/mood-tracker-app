d152d24b2e66b83a5d6da17c334f58eb906700c2
Shahzeb-Abro <khizr772@gmail.com>
2025-07-03 00:03:10 +0500

    Added svg icons as react components for better rendering

---
+++ src/assets/svgAssets/BgPatternAverages.jsx
@@ -0,0 +1,14 @@
+export const BgPatternAverages = ({ width = 243, height = 251 }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill="none"
+      viewBox="0 0 243 251"
+    >
+      <circle cx="110.5" cy="110.5" r="110.5" fill="#fff" opacity=".2" />
+      <circle cx="132.5" cy="140.5" r="110.5" fill="#fff" opacity=".2" />
+    </svg>
+  );
+};
---
+++ src/assets/svgAssets/index.js
@@ -0,0 +1,49 @@
+import { AvatarPlaceholder } from "./AvatarPlaceholder";
+import { BgPatternAverages } from "./BgPatternAverages";
+import { IconCheck } from "./IconCheck";
+import { IconDropdownArrow } from "./IconDropdownArrow";
+import { IconHappyColor } from "./IconHappyColor";
+import { IconHappyWhite } from "./IconHappyWhite";
+import { IconLogout } from "./IconLogout";
+import { IconNeutralColor } from "./IconNeutralColor";
+import { IconNeutralWhite } from "./IconNeutralWhite";
+import { IconQuote } from "./IconQuote";
+import { IconReflection } from "./IconReflection";
+import { IconSadColor } from "./IconSadColor";
+import { IconSadWhite } from "./IconSadWhite";
+import { IconSettings } from "./IconSettings";
+import { IconSleep } from "./IconSleep";
+import { IconTrendDecrease } from "./IconTrendDecrease";
+import { IconTrendIncrease } from "./IconTrendIncrease";
+import { IconTrendSame } from "./IconTrendSame";
+import { IconVeryHappyColor } from "./IconVeryHappyColor";
+import { IconVeryHappyWhite } from "./IconVeryHappyWhite";
+import { IconVerySadColor } from "./IconVerySadColor";
+import { IconVerySadWhite } from "./IconVerySadWhite";
+import { Logo } from "./Logo";
+
+export {
+  Logo,
+  IconVeryHappyColor,
+  IconVeryHappyWhite,
+  IconVerySadColor,
+  IconVerySadWhite,
+  IconTrendSame,
+  IconTrendDecrease,
+  IconTrendIncrease,
+  IconSleep,
+  IconSettings,
+  IconSadColor,
+  IconSadWhite,
+  IconQuote,
+  IconReflection,
+  IconNeutralColor,
+  IconCheck,
+  IconDropdownArrow,
+  IconNeutralWhite,
+  IconLogout,
+  IconHappyColor,
+  IconHappyWhite,
+  BgPatternAverages,
+  AvatarPlaceholder,
+};
