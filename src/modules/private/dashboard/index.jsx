aa55da7dca6752d9700846c07955fee58f704861
Shahzeb-Abro <khizr772@gmail.com>
2025-07-03 23:05:10 +0500

    CI/CD Test

--- src/modules/private/dashboard/index.jsx
+++ src/modules/private/dashboard/index.jsx
@@ -7,7 +7,7 @@ export const Dashboard = () => {
       <Header />
 
       <div className="flex flex-col lg:flex-row max-w-[1170px] mx-auto gap-8 mt-16 pb-20">
-        <div className="flex-1 py-5 px-4 md:py-6 md:px-5 lg:px-6 bg-white rounded-2xl border border-blue-100 flex flex-col gap-6 max-w-[370px]">
+        <div className="flex-1 py-5 px-4 md:py-6 md:px-5 lg:px-6 bg-white rounded-2xl border border-blue-100 flex flex-col gap-6 lg:max-w-[370px]">
           <AverageMood />
           <AverageSleep />
         </div>
